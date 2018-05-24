ff222bfdb69915d79bdc7d4e8be8754339e67ea6
Ender <pavlenko.jaroslav@yandex.ru>
2018-05-24 15:49:23 +0700

    Added IDbContext, modified ServersController

--- GameStatsServer/Controllers/ServersController.cs
+++ GameStatsServer/Controllers/ServersController.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameStatsServer.DataProviders;
+using GameStatsServer.Extensions;
+using GameStatsServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace GameStatsServer.Controllers
 {
@@ -6,46 +14,56 @@ namespace GameStatsServer.Controllers
     [Route("api/Servers")]
     public class ServersController : Controller
     {
+        private IDbContext dbContext;
+        public ServersController(IDbContext dbContext)
+            => this.dbContext = dbContext;
+
         //GET api/servers/info
         [HttpGet("info")]
-        public string AllInfo()
+        public async Task<IEnumerable<ServerInfoWithEndpoint>> AllInfo()
         {
-            return "all info";
+            return await dbContext.Servers.Select(server => server.CreateServerInfoWithEndpoint()).ToListAsync();
         }
 
         //GET api/servers/{endpoint}/info
         [HttpGet("{endpoint}/info")]
-        public object Info(string endpoint)
+        public async Task<ServerInfo> Info(string endpoint)
         {
-            return new { Info = "get info", Endpoint = endpoint };
+            var server = await dbContext.Servers.FindAsync(endpoint);
+            return server?.CreateServerInfo() ?? null;
         }
 
         //PUT api/servers/{endpoint}/info
         [HttpPut("{endpoint}/info")]
-        public void Info(string endpoint, [FromBody]string value)
+        public async void Info(string endpoint, [FromBody]ServerInfo value)
         {
-
+            await dbContext.Servers.AddAsync(value.CreateServer(endpoint));
+            await dbContext.SaveChangesAsync();
         }
 
         //GET api/servers/{endpoint}/stats
         [HttpGet("{endpoint}/stats")]
         public string Stats(string endpoint)
         {
-            return "stats";
+            //TODO: Models.ServerStats
+            throw new NotImplementedException("//TODO: Models.ServerStats");
         }
 
         //GET api/servers/{endpoint}/matches/{timestamp}
         [HttpGet("{endpoint}/matches/{timestamp}")]
-        public object Matches(string endpoint, string timestamp)
+        public async Task<MatchInfo> Match(string endpoint, string timestamp)
         {
-            return new { Info = "get matches", Endpoint = endpoint, Timestamp = timestamp };
+            var match = await dbContext.Matches.FindAsync(endpoint, timestamp);
+            return match?.CreateMatchInfo() ?? null;
         }
 
         //PUT api/servers/{endpoint}/matches/{timestamp}
         [HttpPut("{endpoint}/matches/{timestamp}")]
-        public void Matches(string endpoint, string timestamp, [FromBody]string value)
+        public async void Match(string endpoint, string timestamp, [FromBody]MatchInfo value)
         {
-
+            var server = await dbContext.Servers.FindAsync(endpoint);
+            server?.Mathes.Add(value.CreateMatch(timestamp));
+            await dbContext.SaveChangesAsync();
         }
     }
 }
---
+++ GameStatsServer/DataProviders/IDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GameStatsServer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace GameStatsServer.DataProviders
+{
+    public interface IDbContext : IDisposable
+    {
+        DbSet<Server> Servers { get; }
+        DbSet<Match> Matches { get; }
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+    }
+}
